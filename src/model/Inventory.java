2040061f8cca2b34f01bc8f8e861365da120ce70
Musee Ullah <lae@lae.is>
2020-09-02 22:36:58 -0500

    Initial model definitions

---
+++ src/model/Inventory.java
@@ -0,0 +1,51 @@
+package model;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class Inventory {
+    private static final ObservableList<Part> allParts = FXCollections.observableArrayList();
+    private static final ObservableList<Product> allProducts = FXCollections.observableArrayList();
+
+    public static void addPart(Part newPart) {
+        allParts.add(newPart);
+    }
+
+    /*public static Part lookupPart(int partId) {
+
+    }
+    public static ObservableList<Part> lookupPart(String partName) {
+        
+    }
+    public static void updatePart(int index, Part selectedPart) {
+        
+    }
+    public static boolean deletePart(Part selectedPart) {
+        
+    }*/
+
+    public static ObservableList<Part> getAllParts() {
+        return allParts;
+    }
+
+    public static void addProduct(Product newProduct) {
+        allProducts.add(newProduct);
+    }
+
+    /*public static Product lookupProduct(int productId) {
+
+    }
+    public static ObservableList<Product> lookupProduct(String productName) {
+        
+    }
+    public static void updateProduct(int index, Product selectedProduct) {
+        
+    }
+    public static boolean deleteProduct(Product selectedProduct) {
+        
+    }*/
+
+    public static ObservableList<Product> getAllProducts() {
+        return allProducts;
+    }
+}
